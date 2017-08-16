e03497eba796966ec514096f997ab0d157250780
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-08-16 15:59:12 -0300

    WIP Serialization

---
+++ Assets/AntonioHR/TreeAsset/BehaviorTree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace AntonioHR.TreeAsset
+{
+    public class BehaviorTree: ScriptableObject, ISerializationCallbackReceiver
+    {
+        private List<TreeNode> allNodes;
+
+        private TreeNode root;
+
+
+
+
+        public T CreateNode<T>() where T: TreeNode
+        {
+            var newNode = ScriptableObject.CreateInstance<T>();
+            AssetDatabase.AddObjectToAsset(newNode, this);
+            allNodes.Add(newNode);
+            return newNode;
+        }
+
+        public void DeleteNode(TreeNode node)
+        {
+            foreach (var child in node.Children)
+            {
+                DeleteNode(child);
+            }
+
+            node.BeforeDelete();
+            DestroyImmediate(node);
+        }
+
+
+        public void OnAfterDeserialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnBeforeSerialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    [Serializable]
+    public class SerializedTreeHierachy
+    {
+        SerializableDictionary<TreeNode, List<TreeNode>> hierarchy;
+
+    }
+}
---
+++ Assets/AntonioHR/TreeAsset/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AntonioHR.TreeAsset
+{
+    public abstract class TreeNode : ScriptableObject
+    {
+        protected abstract int MaximumChildrenCount { get; }
+
+        public bool IsLeaf { get { return MaximumChildrenCount == 0; } }
+        public bool IsDecorator { get { return MaximumChildrenCount == 1; } }
+        public bool IsComposite { get { return MaximumChildrenCount > 1; } }
+
+        public abstract IEnumerable<TreeNode> Children { get; }
+        public abstract void AddChild(TreeNode child);
+
+        public abstract void BeforeDelete();
+    }
+}
